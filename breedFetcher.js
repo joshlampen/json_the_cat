3930ff119d9a8e928e5e1c626cdf5cc564123bef
JoshLampen <joshlampen2@gmail.com>
2020-05-06 18:25:39 +0000

    improved error messages

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,7 +5,17 @@ let breed = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(url, (error, response, body) => {
-  if (error) throw error;
+  if (error) {
+    console.log("Error: request failed... check url");
+    process.exit();
+  }
+
   const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.log("Error: breed not found");
+    process.exit();
+  }
+  
   console.log(data[0].description);
 });
